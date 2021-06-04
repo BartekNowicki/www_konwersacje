64746563f90ec8001c78633f12c517830244c0eb
Bartosz Nowicki <65188628+BartekNowicki@users.noreply.github.com>
2021-06-04 22:04:45 +0200

    formik ready but still not sending emails

--- src/components/myFormik.js
+++ src/components/myFormik.js
@@ -3,7 +3,6 @@
  import { Formik, Form, Field, ErrorMessage } from 'formik';
 
  import { useStore } from 'react-redux'
-
  
 
  const BasicFormik = () => {
@@ -19,45 +18,28 @@
         }
     }
 
-    const actionChangeMessage = (newMessage) => {
-        
+    const actionChangeMessage = (newMessage) => {        
         return {
             type: 'NEW_MESSAGE', 
             payload: newMessage
         }
     }
-
      
     return (
-
-   <div>
-
+     <div>
      <h1>Kontakt</h1>
-
      <Formik
-
        initialValues={{ email: '', message: '' }}
-
        validate={values => {
-
          const errors = {};
-
          if (!values.email) {
-
            errors.email = 'wymagane';
-
          } else if (
-
            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-
          ) {
-
            errors.email = 'podaj poprawny email';
-
          }
-
          return errors;
-
        }}
 
        onSubmit={(values, { setSubmitting }) => {
@@ -66,39 +48,25 @@
         store.dispatch(actionChangeMessage(values.message));
 
          setTimeout(() => {
-
            alert(JSON.stringify(values, null, 2));           
            setSubmitting(false);
-
          }, 400);
-
        }}
-
      >
 
        {({ isSubmitting }) => (
-
          <Form>
-
-           <Field type="text" name="email" placeholder = 'komu odpisać?'/>
-
+           <Field type="text" name="email" placeholder = 'komu odpisać?' onKeyUp = {(e) => store.dispatch(actionChangeEmail(e.target.value))}/>
            <ErrorMessage name="email" component="div" />
-
-           <Field type="text" name="message" placeholder = 'skrobnij coś'/>
-
+           <Field type="text" name="message" placeholder = 'skrobnij coś' onKeyUp = {(e) => store.dispatch(actionChangeMessage(e.target.value))} />
            <ErrorMessage name="message" component="div" />
-
            <button type="submit" disabled={isSubmitting}> mejla ślij! </button>
-
          </Form>
-
        )}
-
      </Formik>
-
    </div>
-
  )};
- 
 
  export default BasicFormik;
+
+ 
