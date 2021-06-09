da6b81c6f4861fdd5fc8ff794f39626ac05b9c0f
Bartosz Nowicki <65188628+BartekNowicki@users.noreply.github.com>
2021-06-09 22:36:43 +0200

    fixing sendit bug...

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -14,17 +14,22 @@ const sendEmail = async (submissionJSON, confirmFormSent) => {
 
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
 
-      mode: 'cors', // netlify: cors!!!
+      mode: 'cors', // netlify: cors???
       //mode: 'no-cors', // local dev: no-cors!!!
-      
+
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
+
       headers: {
       'Content-Type': 'application/json'      
       },
+
       redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url      
+
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+
       body:  submissionJSON,
+      
       });
       
       console.log('status: ', status);
