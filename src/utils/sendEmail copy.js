390fc37641c94c338585200d29847d9a2d210d8b
Bartosz Nowicki <65188628+BartekNowicki@users.noreply.github.com>
2021-06-09 16:45:44 +0200

    refactored async await in sendMEmail

---
+++ src/utils/sendEmail copy.js
@@ -0,0 +1,27 @@
+
+const sendEmail = async (submissionJSON, confirmFormSent) => {
+  
+  let result;
+
+  console.log('submission to send ', submissionJSON);
+
+  try {
+      result = await fetch('/.netlify/functions/sendit', {
+      // result = await fetch('https://angielski-konwersacje.eu/.netlify/functions/sendit', {
+      //result = await fetch('http://localhost:8888/.netlify/functions/sendit', {
+      method: 'POST',
+      mode: 'cors',
+      body:  submissionJSON,
+      }).then(data => data.json()).then(data => {
+        console.log('result: ', data.status);
+        data.status === 'success' ? confirmFormSent(true) : confirmFormSent(false);
+      })
+  
+  } catch (error) {
+    console.error('error sending the message...', error);
+  }
+
+}
+
+export default sendEmail
+
--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,21 +1,19 @@
 
 const sendEmail = async (submissionJSON, confirmFormSent) => {
   
-  let result;
-
   console.log('submission to send ', submissionJSON);
 
   try {
-      result = await fetch('/.netlify/functions/sendit', {
-      // result = await fetch('https://angielski-konwersacje.eu/.netlify/functions/sendit', {
-      //result = await fetch('http://localhost:8888/.netlify/functions/sendit', {
+      // const { status } = await fetch('/.netlify/functions/sendit', {
+        
+         const { status } = await fetch('http://localhost:8888/.netlify/functions/sendit', {
+
       method: 'POST',
       mode: 'cors',
       body:  submissionJSON,
-      }).then(data => data.json()).then(data => {
-        console.log('result: ', data.status);
-        data.status === 'success' ? confirmFormSent(true) : confirmFormSent(false);
-      })
+      });
+      console.log('status: ', status);
+      status === 200 ? confirmFormSent(true) : confirmFormSent(false);
   
   } catch (error) {
     console.error('error sending the message...', error);
