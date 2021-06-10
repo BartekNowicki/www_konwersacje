99477c1402c1060a0838b00ec3176e48ad546d31
Bartosz Nowicki <65188628+BartekNowicki@users.noreply.github.com>
2021-06-10 18:43:11 +0200

    switched email to OAuth2, works locally

--- functions/sendit.js
+++ functions/sendit.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 
 
-
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
 
     console.log('serverless handler initiated ');
+
+    const oAuth2Client = new google.auth.OAuth2(process.env.GATSBY_CLIENT_ID, process.env.GATSBY_CLIENT_SECRET_KEY, process.env.GATSBY_REDIRECT_URI);
+    
+    oAuth2Client.setCredentials({ refresh_token: process.env.GATSBY_REFRESH_TOKEN });
+
 
     const showInfo = (intro = 'show info: ', msg = '\n you did not write anything non default to show here...') => {
     console.log('----------------------------------------');
@@ -14,37 +19,45 @@ exports.handler = async (event, context) => {
     console.log('----------------------------------------');
     }
 
+
     const dummyTest = JSON.stringify({ "email": "testEmail", "message": "testMessage" });
+
     
     const mailContent = event.body ? JSON.parse(event.body) : JSON.parse(dummyTest);
 
+
     showInfo('stringified mailContent sent by serverless sendit ', JSON.stringify(mailContent));
+
+
+    const accessToken = await oAuth2Client.getAccessToken();
     
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // use SSL
-        auth: {            
+    
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',          
             user: process.env.GATSBY_EMAIL_LOGIN,
-            pass: process.env.GATSBY_EMAIL_PASS        
+            clientId: process.env.GATSBY_CLIENT_ID,
+            clientSecret: process.env.GATSBY_CLIENT_SECRET_KEY,
+            refreshToken: process.env.GATSBY_REFRESH_TOKEN,
+            accessToken: accessToken,
+            
         }
     });
 
+    const mailOptions = {
+      from: 'bartosznowickihq@gmail.com',
+      to: 'angielskikonwersacjeeu@gmail.com',
+      subject: `you've got awesomness`,
+      text: `what is awesome is this: ${JSON.stringify(mailContent)}`,
+      html: `<p>what is awesome is this: ${JSON.stringify(mailContent)}</p>`
+    };
+
   try {
 
-    transporter.sendMail({
-        from: 'bartosznowickihq@gmail.com',
-        // from: 'angielskikonwersacjeeu@gmail.com',
-        to: 'angielskikonwersacjeeu@gmail.com',
-        subject: `you've got mail :)`,
-        html: `<p>${JSON.stringify(mailContent)}</p>`
-    }, function(error, info) {        
-       if (error) {
-           showInfo('error', error);
-       } else {
-           showInfo('goodInfo', info);
-       } 
-    });
+    const result = await transport.sendMail(mailOptions);
+    
+    showInfo('result ', result);
 
     return {
     statusCode: 200,
