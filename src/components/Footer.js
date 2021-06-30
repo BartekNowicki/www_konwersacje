25cc5d6503ca591523e57bb1c7aa1ad5b1268a81
Bartosz Nowicki <65188628+BartekNowicki@users.noreply.github.com>
2021-06-30 10:51:46 +0200

    added image attributions in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import * as scssVariables from '../scssVariablesForJs.module.scss'
 
 
 const Footer = () => (
@@ -7,14 +8,28 @@ const Footer = () => (
       //border: '1px solid gray',
       padding: '2vw',
       display: 'flex',
-      justifyContent: 'center'
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
       }}>
+
       <p style={{
       color: 'gray',
       fontSize: '2vmin',
-      padding: '3vw',
-      fontFamily: 'Roboto Condensed'  
+      padding: '1vmin',
+      fontFamily: scssVariables.scssSecondaryFont,
       }}>© 2001 - {new Date().getFullYear()} Bardo Bartosz Nowicki Center For Conversational Language Development</p>
+
+      <p style={{
+      color: 'gray',
+      fontSize: '2vmin',
+      padding: '1vmin',
+      fontFamily: scssVariables.scssSecondaryFont,
+      }}>Image attributions: <a href="http://www.freepik.com">Designed by Freepik</a>&nbsp;and&nbsp;
+      <a href='https://www.freepik.com/vectors/car'>Car vector created by upklyak - www.freepik.com</a>
+      </p>
+
+
   </footer>
 )
 
